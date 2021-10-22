4f30b328ba068824edc04288ddc9f058f5d7c29a
Nihal-a <nihalavulan1@gamil.com>
2021-10-22 12:15:38 +0530

    company unverified page name issue fied

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -15,7 +15,7 @@ function Dashboard() {
     const UnVerify = () => {
         return(
             <div className="flex flex-col items-center justify-center h-screen">
-                <h1 className="text-6xl font-semibold ">Hey,<span className="text-primary">Google</span></h1>
+                <h1 className="text-6xl font-semibold ">Hey <span className="text-primary">{company.company.companyName},</span></h1>
                 <h2 className="text-4xl mt-4">Your Company has Created Successfully.</h2>
                 <p className="text-center mt-6 text-2xl font-light">JobsWay will Verify Your Company and Provide the <br /> Dashboard to you within 1 - 2 days</p>
                 <a href="" className="underline text-sm mt-8 text-primary">Back to login</a>
